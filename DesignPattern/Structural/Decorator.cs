ff1a8ab08e8af2d160aae27d4a694da81ac98a7e
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2021-10-05 12:07:39 +0200

    Add the Decorator pattern

---
+++ DesignPattern/Structural/Decorator.cs
@@ -0,0 +1,45 @@
+﻿namespace DesignPattern.Structural
+{
+    //Decorator lets you attach new behaviors to objects by placing these objects inside special wrapper objects that contain the behaviors.
+
+    public interface IDecoProduct
+    {
+        void DisplayInfo();
+    }
+
+    public class DecoProduct : IDecoProduct
+    {
+        public void DisplayInfo()
+        {
+            System.Console.WriteLine("Product information");
+        }
+    }
+
+    public record StandardDecorator(IDecoProduct Product) : IDecoProduct
+    {
+        public virtual void DisplayInfo()
+        {
+            Product.DisplayInfo();
+        }
+    }
+
+    public record PreInfoDecorator(IDecoProduct Product)
+        : StandardDecorator(Product)
+    {
+        public override void DisplayInfo()
+        {
+            System.Console.WriteLine("Pre-Information");
+            base.DisplayInfo();
+        }
+    }
+
+    public record PostInfoDecorator(IDecoProduct Product)
+        : StandardDecorator(Product)
+    {
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            System.Console.WriteLine("Post-Information");
+        }
+    }
+}
