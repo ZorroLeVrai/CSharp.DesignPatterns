8e0ddc31b6f9409f86a51c1bfd4b9bb50996e335
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2021-10-07 16:38:21 +0200

    Machine State pattern add encapsulation

--- DesignPattern/Others/StateMachine.cs
+++ DesignPattern/Others/StateMachine.cs
@@ -4,7 +4,9 @@ using System.Linq;
 
 namespace DesignPattern.Others
 {
-    /* 
+    /*
+     * This exercice has been taken from the following website
+     * https://fsharpforfunandprofit.com/posts/designing-for-correctness/
     Let’s say that you are designing an e-commerce site which has a shopping cart and you are given the following requirements.
     * You can only pay for a cart once.
     * Once a cart is paid for, you cannot change the items in it.
@@ -12,94 +14,11 @@ namespace DesignPattern.Others
 
     public record Product(string Name, double Price);
 
-    public interface ICart
-    {
-        string GetDisplay();
-    }
-
-    public interface IAddProduct
-    {
-        ICart AddProduct(Product product);
-    }
-
-    public interface IRemoveProduct
-    {
-        ICart RemoveProduct(Product product);
-    }
-
-    public interface IPayCart
-    {
-        ICart Pay();
-    }
-
-    public class EmptyCart : ICart, IAddProduct
-    {
-        public ICart AddProduct(Product product)
-        {
-            return new ActiveCart(new List<Product> { product });
-        }
-
-        public string GetDisplay()
-            => "Empty Cart";
-    }
-
-    public class ActiveCart : ICart, IAddProduct, IPayCart, IRemoveProduct
-    {
-        public IEnumerable<Product> UnpaidItems { get; } = new List<Product>();
-
-        public ActiveCart(IEnumerable<Product> products)
-        {
-            UnpaidItems = products;
-        }
-
-        public ICart AddProduct(Product product)
-        {
-            return new ActiveCart(new List<Product>(UnpaidItems) { product });
-        }
-
-        public ICart Pay()
-        {
-            return new PaidCart(UnpaidItems);
-        }
-
-        public string GetDisplay()
-            => $"Active Cart: [{string.Join(',', UnpaidItems)}]";
-
-        public ICart RemoveProduct(Product product)
-        {
-            var remainingItems = UnpaidItems.Where(item => item != product);
-
-            if (remainingItems.Any())
-                return new ActiveCart(remainingItems);
-
-            return new EmptyCart();
-        }
-    }
-
-    public class PaidCart : ICart
-    {
-        public IEnumerable<Product> PaidItems { get; } = new List<Product>();
-
-        public double Amount { get; } = 0;
-
-        public PaidCart(IEnumerable<Product> products)
-        {
-            PaidItems = products;
-            Amount = products
-                .Select(product => product.Price)
-                .Sum();
-                //.Aggregate((a, e) => a + e);
-        }
-
-        public string GetDisplay()
-            => $"Paid Cart: (Amount: {Amount}) [{string.Join(',', PaidItems)}]";
-    }
-
     public class ShoppingCart
     {
         private readonly ICart _cart;
 
-        public ShoppingCart(ICart cart)
+        private ShoppingCart(ICart cart)
             => _cart = cart;
 
         public ShoppingCart()
@@ -112,6 +31,7 @@ namespace DesignPattern.Others
             if (_cart is IAddProduct cartAdder)
                 return new ShoppingCart(cartAdder.AddProduct(product));
 
+            Console.WriteLine("Unable to add product");
             return this;
         }
 
@@ -120,6 +40,7 @@ namespace DesignPattern.Others
             if (_cart is IRemoveProduct cartRemover)
                 return new ShoppingCart(cartRemover.RemoveProduct(product));
 
+            Console.WriteLine("Unable to remove product");
             return this;
         }
 
@@ -128,11 +49,95 @@ namespace DesignPattern.Others
             if (_cart is IPayCart cartPayer)
                 return new ShoppingCart(cartPayer.Pay());
 
+            Console.WriteLine("Unable to pay");
             return this;
         }
 
         public void Display()
             => Console.WriteLine(_cart.GetDisplay());
+
+        private interface ICart
+        {
+            string GetDisplay();
+        }
+
+        private interface IAddProduct
+        {
+            ICart AddProduct(Product product);
+        }
+
+        private interface IRemoveProduct
+        {
+            ICart RemoveProduct(Product product);
+        }
+
+        private interface IPayCart
+        {
+            ICart Pay();
+        }
+
+        private class EmptyCart : ICart, IAddProduct
+        {
+            public ICart AddProduct(Product product)
+            {
+                return new ActiveCart(new List<Product> { product });
+            }
+
+            public string GetDisplay()
+                => "Empty Cart";
+        }
+
+        private class ActiveCart : ICart, IAddProduct, IPayCart, IRemoveProduct
+        {
+            public IEnumerable<Product> UnpaidItems { get; } = new List<Product>();
+
+            public ActiveCart(IEnumerable<Product> products)
+            {
+                UnpaidItems = products;
+            }
+
+            public ICart AddProduct(Product product)
+            {
+                return new ActiveCart(new List<Product>(UnpaidItems) { product });
+            }
+
+            public ICart Pay()
+            {
+                return new PaidCart(UnpaidItems);
+            }
+
+            public string GetDisplay()
+                => $"Active Cart: [{string.Join(',', UnpaidItems)}]";
+
+            public ICart RemoveProduct(Product product)
+            {
+                var remainingItems = UnpaidItems.Where(item => item != product);
+
+                if (remainingItems.Any())
+                    return new ActiveCart(remainingItems);
+
+                return new EmptyCart();
+            }
+        }
+
+        private class PaidCart : ICart
+        {
+            public IEnumerable<Product> PaidItems { get; } = new List<Product>();
+
+            public double Amount { get; } = 0;
+
+            public PaidCart(IEnumerable<Product> products)
+            {
+                PaidItems = products;
+                Amount = products
+                    .Select(product => product.Price)
+                    .Sum();
+                //.Aggregate((a, e) => a + e);
+            }
+
+            public string GetDisplay()
+                => $"Paid Cart: (Amount: {Amount}) [{string.Join(',', PaidItems)}]";
+        }
     }
 
     public class CartClient
@@ -144,6 +149,9 @@ namespace DesignPattern.Others
 
             var biscuit = new Product("Biscuit", 2.5);
             var fromage = new Product("Fetta", 4.1);
+
+            cart = cart.RemoveProduct(biscuit);
+            cart.Display();
 
             cart = cart.AddProduct(biscuit);
             cart.Display();
